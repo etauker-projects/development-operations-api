a49c3744607d13e5fa13b29b5f67ced24ea13104
Tautvydas Kersulis <etauker@gmail.com>
2022-08-04 20:27:12 +0100

    Add test placeholders

---
+++ service/src/schemas/schema.service.spec.ts
@@ -0,0 +1,29 @@
+import 'mocha';
+
+describe('SchemaService', () => {
+
+    describe('initialiseSchema', () => {
+        xit('should create a schema', () => {
+            // TODO: implement
+        });
+        xit('should create a schema admin', () => {
+            // TODO: implement
+        });
+        xit('should create a schema user', () => {
+            // TODO: implement
+        });
+    });
+
+    describe('removeSchema', () => {
+        xit('should delete a schema', () => {
+            // TODO: implement
+        });
+        xit('should delete a schema admin', () => {
+            // TODO: implement
+        });
+        xit('should delete a schema user', () => {
+            // TODO: implement
+        });
+    });
+
+});
