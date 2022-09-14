7d786bc2afcb4ee5b7bd306777522d3b1f61403d
Tautvydas Kersulis <etauker@gmail.com>
2022-09-14 22:28:54 +0100

    Create nginx setup file

---
+++ tools/ubuntu/scripts/setup.sh
@@ -0,0 +1,12 @@
+echo 'Installing required utilities'
+echo '---'
+sudo apt-get install apache2-utils -y
+
+echo 'Exposing ssh (port 22) through firewall'
+echo '---'
+sudo ufw allow ssh
+
+echo 'Generating nginx basic-auth file'
+echo '---'
+# TODO: decide if this should be changed to Ubuntu
+htpasswd -c `pwd`/mnt/.htpasswd etauker
