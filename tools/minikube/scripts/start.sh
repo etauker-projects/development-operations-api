cf64184f2dbe1cb9028d66c1d7415a56e449e439
Tautvydas Kersulis <etauker@gmail.com>
2022-09-15 00:24:15 +0100

    Add a note

--- tools/minikube/scripts/start.sh
+++ tools/minikube/scripts/start.sh
@@ -5,6 +5,7 @@ echo '---'
 minikube start
 
 # # TODO: find out why this needs a password
+# # NOTE: until then, tunnel might need to be started manually
 # echo 'Starting minikube tunnel'
 # echo '---'
 # nohup minikube tunnel -c --rootless &> ./tools/minikube/mnt/minikube-tunnel.out &
