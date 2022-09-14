f5cfea4437eccb4f3645130c95493e5689a3f831
Tautvydas Kersulis <etauker@gmail.com>
2022-09-15 00:22:32 +0100

    Break up nginx.conf

--- tools/minikube/scripts/start.sh
+++ tools/minikube/scripts/start.sh
@@ -3,3 +3,8 @@
 echo 'Starting minikube cluster'
 echo '---'
 minikube start
+
+# # TODO: find out why this needs a password
+# echo 'Starting minikube tunnel'
+# echo '---'
+# nohup minikube tunnel -c --rootless &> ./tools/minikube/mnt/minikube-tunnel.out &
--- tools/nginx/scripts/start.sh
+++ tools/nginx/scripts/start.sh
@@ -1,19 +1,44 @@
 # assumes the script to be run from root directory
 cd ./tools/nginx
 
+echo 'Dynamically generating minikube.conf file'
+echo '---'
+cat <<EOF > `echo $(pwd)/mnt`/conf.d/minikube.conf
+server {
+
+    listen       8443;
+
+    auth_basic "Administrator’s Area";
+    auth_basic_user_file /etc/nginx/.htpasswd;
+  
+    location / {
+        proxy_pass https://`minikube ip`:8443;
+        proxy_ssl_certificate /etc/nginx/certs/minikube-client.crt;
+        proxy_ssl_certificate_key /etc/nginx/certs/minikube-client.key;
+    }
+}
+EOF
+
 echo 'Starting nginx-proxy container'
 echo '---'
 docker run \
   --name nginx-proxy \
   -p 8000:80 \
+  -p 8443:8443 \
   -d \
-  -v `pwd`/mnt/:/etc/nginx \
+  -v `pwd`/mnt/.htpasswd:/etc/nginx/.htpasswd \
+  -v `pwd`/mnt/nginx.conf:/etc/nginx/nginx.conf \
+  -v `pwd`/mnt/conf.d/:/etc/nginx/conf.d \
+  -v /home/etauker/.minikube/profiles/minikube/client.key:/etc/nginx/certs/minikube-client.key \
+  -v /home/etauker/.minikube/profiles/minikube/client.crt:/etc/nginx/certs/minikube-client.crt \
   --net="host" \
   --rm nginx
 
-echo 'Exposing port 8000 through firewall'
+echo 'Exposing ports through firewall'
 echo '---'
 sudo ufw allow 8000
+sudo ufw allow 8443
 
-# http://localhost:8000/#/workloads?namespace=default - outside service
+# http://localhost:8000/#/workloads?namespace=default - outside server
 # http://dev.etauker.ie/#/workloads?namespace=default - inside server
+# http://localhost:8443 - outside server
