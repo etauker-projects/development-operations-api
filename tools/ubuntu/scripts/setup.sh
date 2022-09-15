e1b6904fe32c48c9a67c89f1dcd5c7a3c8e52d69
Tautvydas Kersulis <etauker@gmail.com>
2022-09-15 01:14:01 +0100

    Update Ubuntu setup script to generate password

--- tools/ubuntu/scripts/setup.sh
+++ tools/ubuntu/scripts/setup.sh
@@ -1,3 +1,5 @@
+# assumes the script to be run from root directory
+
 echo 'Installing required utilities'
 echo '---'
 sudo apt-get install apache2-utils -y
@@ -8,5 +10,6 @@ sudo ufw allow ssh
 
 echo 'Generating nginx basic-auth file'
 echo '---'
-# TODO: decide if this should be changed to Ubuntu
-htpasswd -c `pwd`/mnt/.htpasswd etauker
+password=`head /dev/urandom | tr -dc A-Za-z0-9 | head -c32`
+htpasswd -bc `pwd`/tools/nginx/mnt/.htpasswd etauker-dev $password
+echo "etauker-dev=$password"
